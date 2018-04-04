1de522781e48d16ef00f45e491945426b2c1ee93
feven <feventadesse2@gmail.com>
2018-04-04 13:25:00 -0700

    comment added

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -24,6 +24,7 @@ class HomeController extends Controller
      */
     public function index()
     {
+        //this comment added
         $user_id = auth()->user()->id;
         $user = User::find($user_id);
         return view('home')->with('posts',$user->posts);
